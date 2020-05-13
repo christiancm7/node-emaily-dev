6fe39c3e6a58b278a4caafa2c69151a9618e9f50
Christian <atticuschris@yahoo.com>
2020-05-13 09:57:28 -0500

    full cookie auth setup done

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
 require("./services/passport");
 
+// Mongoose Atlas DB Connection
+mongoose
+  .connect(keys.mongoURI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DB Connected!"))
+  .catch((err) => {
+    console.log(`DB Connection Error: ${err.message}`);
+  });
 const app = express();
+
+app.use(
+  cookieSession({
+    // 30 days time limit
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+//Init the session
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,4 +8,14 @@ module.exports = (app) => {
     })
   );
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get("/api/current_user", (req, res) => {
+    // user object auto passed from passport
+    res.send(req.user);
+  });
 };
